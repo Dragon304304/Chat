d8ce2f5b856aa293f0c8d4239193e2478c5bb716
Dragon304304 <99217854+Dragon304304@users.noreply.github.com>
2023-10-19 20:27:21 +0000

    Cleaned up code, adapted to linux environment.

--- main.py
+++ main.py
@@ -1,148 +1,142 @@
-# Sorry for the delay. No idea how to download msvcrt on replit but the code works when I run it through cmd.
+# not really sure if it properly runs on replit, but i'm pretty sure the code is fine
+# for now the code is still made to work for multiple clients who don't know each other's ip
 
-import msvcrt
 import select
 import socket
 import sys
 
-
-def setup_sockets():
-  global BOUND_SOCKET
-  global PORT
-  global USERNAME
-  global BROADCASTER
-  BOUND_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  BOUND_SOCKET.bind((IP, 0))
-  BOUND_SOCKET.listen()
-  PORT = BOUND_SOCKET.getsockname()[1]
-  USERNAME = input("Please enter a username:\n")
-  BROADCASTER = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
-                              socket.IPPROTO_UDP)
-  request_chat_ports()
+RECV_SIZE = 1024
 
 
-def request_chat_ports():
-  BROADCASTER.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-  BROADCASTER.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-  BROADCASTER.bind(('', 37020))
-  BROADCASTER.sendto(f"^join|{PORT}|{USERNAME}|1|0".encode('ascii'),
-                     ('<broadcast>', 37020))
-  BROADCASTER.recv(1024).decode('ascii')
+class ChatUser:
 
+  def __init__(self):
+    self.bound_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.bound_socket.bind(('0.0.0.0', 0))
+    self.bound_socket.listen()
+    self.broadcaster = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+                                     socket.IPPROTO_UDP)
+    self.username = input("Please enter a username:\n")
+    self.username_dict = {}
+    self.recv_users = []
+    self.send_users = []
+    self.messages_to_send = []
+    self.text_msg = ""
+    self.request_chat_ports()
 
-def run_chat():
-  while True:
-    manage_text_msg()
-    rlist, wlist, xlist = select.select([BOUND_SOCKET] + RECV_USERS,
-                                        SEND_USERS, [], 0)
-    rlist += select.select([BROADCASTER], [], [], 0)[0]
-    recv_messages(rlist)
-    send_messages(wlist)
+  def request_chat_ports(self):
+    self.broadcaster.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    self.broadcaster.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    self.broadcaster.bind(('', 37020))
+    self.broadcaster.sendto(
+        f"^join|{socket.gethostbyname(socket.gethostname())}|{self.bound_socket.getsockname()[1]}|{self.username}|1|0"
+        .encode('ascii'), ('<broadcast>', 37020))
+    self.broadcaster.recv(RECV_SIZE).decode('ascii')
 
+  def run_chat(self):
+    while True:
+      self.manage_text_msg()
+      rlist, wlist, xlist = select.select(
+          [self.bound_socket] + self.recv_users, self.send_users, [], 0)
+      rlist += select.select([self.broadcaster], [], [], 0)[0]
+      self.recv_messages(rlist)
+      self.send_messages(wlist)
 
-def manage_text_msg():
-  global TEXT_MSG
-  if not msvcrt.kbhit():
-    return
-  letter = msvcrt.getch().decode('ascii')
-  if letter == '\r':
-    if TEXT_MSG == "":
+  def manage_text_msg(self):
+    """if not msvcrt.kbhit():
       return
-    if TEXT_MSG.lower() == "/quit":
-      terminate_self()
-    print(f"{USERNAME} (You) -> {TEXT_MSG}")
-    MESSAGES_TO_SEND.append((None, f"^msg|{TEXT_MSG}"))
-    TEXT_MSG = ""
-  elif letter == '\b':
-    if len(TEXT_MSG) > 0:
-      TEXT_MSG = TEXT_MSG[:-1]
-  else:
-    TEXT_MSG += letter
-
-
-def recv_messages(rlist):
-  for current_socket in rlist:
-    if current_socket is BOUND_SOCKET:
-      new_socket, address = BOUND_SOCKET.accept()
-      RECV_USERS.append(new_socket)
+    letter = msvcrt.getch().decode('ascii')"""
+    readable, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if not readable:
+      return
+    letter = sys.stdin.read(1)
+    print(f"ch={letter}")
+    if letter == '\r':
+      if self.text_msg == "":
+        return
+      if self.text_msg.lower() == "/quit":
+        self.terminate_self()
+      print(f"{self.username} (You) -> {self.text_msg}")
+      self.messages_to_send.append((None, f"^msg|{self.text_msg}"))
+      self.text_msg = ""
+    elif letter == '\b':
+      if len(self.text_msg) > 0:
+        self.text_msg = self.text_msg[:-1]
     else:
-      handle_message(current_socket)
+      self.text_msg += letter
 
+  def recv_messages(self, rlist):
+    for current_socket in rlist:
+      if current_socket is self.bound_socket:
+        new_socket, address = self.bound_socket.accept()
+        self.recv_users.append(new_socket)
+      else:
+        self.handle_message(current_socket)
 
-def handle_message(user_socket):
-  recv_data = user_socket.recv(1024).decode('ascii').split('|')
-  if recv_data[0] == '':
-    close_connection(user_socket)
-  elif recv_data[0] == '^join':
-    manage_new_user(user_socket, int(recv_data[1]), recv_data[2],
-                    int(recv_data[3]), int(recv_data[4]))
-  elif recv_data[0] == '^msg':
-    print(f"{USERNAME_DICT[user_socket]} -> {recv_data[1]}")
+  def handle_message(self, user_socket):
+    recv_data = user_socket.recv(RECV_SIZE).decode('ascii').split('|')
+    if recv_data[0] == '':
+      self.close_connection(user_socket)
+    elif recv_data[0] == '^join':
+      self.manage_new_user(user_socket, recv_data[1], int(recv_data[2]),
+                           recv_data[3], int(recv_data[4]), int(recv_data[5]))
+    elif recv_data[0] == '^msg':
+      print(f"{self.username_dict[user_socket]} -> {recv_data[1]}")
 
-
-def manage_new_user(og_socket, port, username, connect_to_socket,
-                    save_new_user):
-  if connect_to_socket:
-    new_socket = open_connection(port)
+  def manage_new_user(self, og_socket, ip, port, username, connect_to_socket,
+                      save_new_user):
+    if connect_to_socket:
+      new_socket = self.open_connection(ip, port)
+      if save_new_user:
+        self.messages_to_send.append((
+            new_socket,
+            f"^join|{socket.gethostbyname(socket.gethostname())}|{self.bound_socket.getsockname()[1]}|{self.username}|0|1"
+        ))
+      else:
+        self.messages_to_send.append((
+            new_socket,
+            f"^join|{socket.gethostbyname(socket.gethostname())}|{self.bound_socket.getsockname()[1]}|{self.username}|1|1"
+        ))
     if save_new_user:
-      MESSAGES_TO_SEND.append((new_socket, f"^join|{PORT}|{USERNAME}|0|1"))
-    else:
-      MESSAGES_TO_SEND.append((new_socket, f"^join|{PORT}|{USERNAME}|1|1"))
-  if save_new_user:
-    USERNAME_DICT[og_socket] = username
-    print(f"{username} has joined the chat.")
+      self.username_dict[og_socket] = username
+      print(f"{username} has joined the chat.")
 
+  def open_connection(self, ip, port):
+    new_socket = socket.socket()
+    new_socket.connect((ip, port))
+    self.send_users.append(new_socket)
+    return new_socket
 
-def open_connection(port):
-  new_socket = socket.socket()
-  new_socket.connect(("127.0.0.1", port))
-  SEND_USERS.append(new_socket)
-  return new_socket
+  def close_connection(self, user_socket):
+    if user_socket in self.recv_users:
+      self.recv_users.remove(user_socket)
+    if user_socket in self.send_users:
+      self.send_users.remove(user_socket)
+    if user_socket in self.username_dict:
+      print(f"{self.username_dict[user_socket]} has left the chat.")
+    user_socket.close()
 
+  def send_messages(self, wlist):
+    messages_to_remove = []
+    for message in self.messages_to_send:
+      for current_socket in wlist:
+        if current_socket == message[0] or message[0] is None:
+          send_data = message[1].encode('ascii')
+          current_socket.send(send_data)
+          messages_to_remove.append(message)
+    for message in messages_to_remove:
+      if message in self.messages_to_send:
+        self.messages_to_send.remove(message)
 
-def close_connection(user_socket):
-  if user_socket in RECV_USERS:
-    RECV_USERS.remove(user_socket)
-  if user_socket in SEND_USERS:
-    SEND_USERS.remove(user_socket)
-  print(f"{USERNAME_DICT[user_socket]} has left the chat.")
-  user_socket.close()
-
-
-def send_messages(wlist):
-  for message in MESSAGES_TO_SEND:
-    for current_socket in wlist:
-      if current_socket == message[0] or message[0] is None:
-        send_data = message[1].encode('ascii')
-        current_socket.send(send_data)
-        MESSAGES_TO_REMOVE.append(message)
-  for message in MESSAGES_TO_REMOVE:
-    if message in MESSAGES_TO_SEND:
-      MESSAGES_TO_SEND.remove(message)
-
-
-def terminate_self():
-  BOUND_SOCKET.shutdown(socket.SHUT_RDWR)
-  BOUND_SOCKET.close()
-  sys.exit()
-
-
-IP = '0.0.0.0'
-PORT = 0
-USERNAME = ""
-global BOUND_SOCKET
-global BROADCASTER
-RECV_USERS = []
-SEND_USERS = []
-USERNAME_DICT = {}
-MESSAGES_TO_SEND = []
-MESSAGES_TO_REMOVE = []
-TEXT_MSG = ""
+  def terminate_self(self):
+    self.bound_socket.shutdown(socket.SHUT_RDWR)
+    self.bound_socket.close()
+    sys.exit()
 
 
 def main():
-  setup_sockets()
-  run_chat()
+  my_chat = ChatUser()
+  my_chat.run_chat()
 
 
 if __name__ == "__main__":
